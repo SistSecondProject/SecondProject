eaf913f5220017b40e64495296a9d03225796b87
sist <sist@sist48>
2018-05-04 14:26:11 +0900

    company

--- Second/src/com/sist/change/JspChange.java
+++ Second/src/com/sist/change/JspChange.java
@@ -3,7 +3,7 @@ package com.sist.change;
 public class JspChange {
    private static String[] jsp={
 	   "home.jsp",
-	   "",
+	   "../company/company.jsp",
 	   "../compare/catchcompare.jsp"
    };
    public static String change(int no)
